d233d235479a5d80ec0946374750b8533c409644
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-10-29 23:19:04 +0100

    fix: searching project file in working directory

--- Sources/XcodeProjectCLI/Core/Project.swift
+++ Sources/XcodeProjectCLI/Core/Project.swift
@@ -13,10 +13,8 @@ final class Project {
     init(projectPath: String?) throws {
         var projectPath = projectPath
 
-        if projectPath == nil {
-            let fileManager = FileManager.default
-            let currentDir = fileManager.currentDirectoryPath
-            let contents = try fileManager.contentsOfDirectory(atPath: currentDir)
+        if projectPath == nil, let currentDir = ProcessInfo.processInfo.environment["PWD"] {
+            let contents = try FileManager.default.contentsOfDirectory(atPath: currentDir)
             projectPath = contents.first { $0.hasSuffix(".xcodeproj") }
             projectPath = InputPath(projectPath ?? "", projectRoot: currentDir).absolutePath
         }
