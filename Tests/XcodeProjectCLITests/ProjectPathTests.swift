2fdc2941f3e7f0152d695cdddbc5433bf3f1a834
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-11-14 22:01:57 +0100

    feat: add support for relative xcodeproj path (#6)

---
+++ Tests/XcodeProjectCLITests/ProjectPathTests.swift
@@ -0,0 +1,81 @@
+import Foundation
+import Testing
+@testable import XcodeProjectCLI
+
+extension SerializedSuite {
+    @Suite("Project Path", .serialized)
+    final class ProjectPathTests: ProjectTests {
+        let file = Files.XcodebuildNvimApp.Modules.Main.mainViewModel
+        let relativePath = ".test/XcodebuildNvimApp/XcodeBuildNvimApp.xcodeproj"
+        let dotPath = "../XcodeProjectCLI/.test/XcodebuildNvimApp/XcodeBuildNvimApp.xcodeproj"
+
+        var currentDir: String {
+            ProcessInfo.processInfo.environment["PWD"]
+                ?? #filePath
+                .components(separatedBy: "/")
+                .dropLast(3)
+                .joined(separator: "/")
+        }
+
+        var tildePath: String {
+            testXcodeprojPath.replacingOccurrences(
+                of: NSHomeDirectory(),
+                with: "~"
+            )
+        }
+    }
+}
+
+extension SerializedSuite.ProjectPathTests {
+    @Test
+    func projectPath_shouldListTargets_whenPathContainsDots() throws {
+        var sut = try ListTargetsCommand.parse([
+            dotPath,
+            "--file",
+            file
+        ])
+
+        let output = try runTest(for: &sut)
+
+        #expect(output == ["XcodebuildNvimApp"])
+    }
+
+    @Test
+    func projectPath_shouldListTargets_whenPathContainsTilde() throws {
+        var sut = try ListTargetsCommand.parse([
+            tildePath,
+            "--file",
+            file
+        ])
+
+        let output = try runTest(for: &sut)
+
+        #expect(output == ["XcodebuildNvimApp"])
+    }
+
+    @Test
+    func projectPath_shouldListTargets_whenPathIsRelative() throws {
+        var sut = try ListTargetsCommand.parse([
+            relativePath,
+            "--file",
+            file
+        ])
+
+        let output = try runTest(for: &sut)
+
+        #expect(output == ["XcodebuildNvimApp"])
+    }
+
+    @Test
+    func projectPath_shouldListTargets_whenPathIsAbsolute() throws {
+        var sut = try ListTargetsCommand.parse([
+            testXcodeprojPath,
+            "--file",
+            file
+        ])
+
+        let output = try runTest(for: &sut)
+
+        #expect(output == ["XcodebuildNvimApp"])
+    }
+}
